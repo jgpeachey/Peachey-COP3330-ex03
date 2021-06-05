46e42ff70d7c747766c540c6ea6bee610a28e5b6
Peachey-Desktop <the1peachey@gmail.com>
2021-06-05 13:11:48 -0400

    fixed challenge comments

--- src/main/java/Challenge/App.java
+++ src/main/java/Challenge/App.java
@@ -6,25 +6,24 @@
 package Challenge;
 
 /*
-Create a program that prompts for an input string and displays output that shows the input string and the number of characters the string contains.
+Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
 
 Example Output:
-What is the input string? Homer
-Homer has 5 characters.
+What is the quote? These aren't the droids you're looking for.
+Who said it? Obi-Wan Kenobi
+Obi-Wan Kenobi says, "These aren't the droids you're looking for."
 
 Constraints:
-*Be sure the output contains the original string.
-*Use a single output statement to construct the output.
-*Use a built-in function of the programming language to determine
-the length of the string.
+*Use a single output statement to produce this output, using appropriate
+string-escaping techniques for quotes.
+*If your language supports string interpolation or string substitution,
+don’t use it for this exercise. Use string concatenation instead.
 
-Challenges:
-*If the user enters nothing, state that the user must enter something into
-the program.
-*Implement this program using a graphical user interface and update the
-character counter every time a key is pressed. If your language doesn’t
-have a particularly friendly GUI library, try doing this exercise with
-HTML and JavaScript instead.
+Challenge:
+Modify this program so that instead of prompting for quotes from the user,
+you create a structure that holds quotes and their associated attributions
+and then display all of the quotes using the format in the example. An array
+of maps would be a good choice.
  */
 
 public class App {
